ecde4dd447724be2a86843873f5be24079e49b2e
raymondseyi <seyidami13@gmail.com>
2024-09-12 10:08:23 +0100

    connect to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import CreateProduct from './pages/CreateProduct'
-
 const App = () => {
   return (
     <>
--- src/pages/CreateProduct.jsx
+++ src/pages/CreateProduct.jsx
@@ -1,8 +1,10 @@
 import { useFormik } from "formik";
 import React from "react";
 import * as yup from "yup";
+import axios from "axios"
 // 1. create the form, get data from the form, submit the form, validate the form.
 const CreateProduct = () => {
+  let url = "http://localhost:5002/product/createproduct"
   const formik = useFormik({
     initialValues: {
       productName: "",
@@ -12,16 +14,23 @@ const CreateProduct = () => {
     },
     onSubmit: (values) => {
       console.log(values);
+      axios.post(url,values)
+      .then((response)=>{
+        console.log(response)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
     },
     validationSchema: yup.object({
       productName: yup.string().required("It is required, fill it jhor"),
       productPrice: yup.number().required("It is required, fill it jhor"),
       description: yup.string().required("It is required, fill it jhor"),
       image: yup.string().required("It is required, fill it jhor"),
-      password: yup.string().pattern("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$","Minimum eight characters, at least one letter, one number and one special character").min(6,"error message")
+      // password: yup.string().pattern("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$","Minimum eight characters, at least one letter, one number and one special character").min(6,"error message")
     }),
   });
-  console.log(formik);
+  // console.log(formik);
   return (
     <>
       <h1>CreateProduct</h1>
@@ -63,7 +72,7 @@ const CreateProduct = () => {
           className={formik.touched.image && formik.errors.image ? "form-control my-2 w-100 is-invalid": "form-control my-2 w-100"}
         />
         <small className="text-danger">{formik.touched.image && formik.errors.image}</small>
-        <button disabled={true} type="submit">Save Product</button>
+        <button type="submit">Save Product</button>
       </form>
     </>
   );
